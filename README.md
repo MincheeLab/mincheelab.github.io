0f9865223420b606d0686068f30b056919252c1b
Gerald Estadieu <gestadieu@usj.edu.mo>
2015-04-11 10:19:32 +0800

    start material design WIP

--- README.md
+++ README.md
@@ -24,3 +24,34 @@ cd ..
 git commit -a -m "your commit message here..."
 git push
 ```
+
+
+old bower (bootstrap based)
+{
+  "name": "src",
+  "private": true,
+  "dependencies": {
+    "bootstrap-sass-official": "3.2.0+2",
+    "modernizr": "2.8.3",
+    "jquery": "2.1.1",
+    "font-awesome": "4.2.0",
+    "jquery.easing": "~1.3.0",
+    "angular": "~1.2.24",
+    "angular-route": "~1.2.24",
+    "angular-sanitize": "~1.2.24",
+    "angular-animate": "~1.2.24",
+    "angular-cookies": "~1.2.24",
+    "angular-ui-router": "~0.2.11",
+    "restangular": "~1.4.0",
+    "angular-bootstrap": "~0.11.0",
+    "ng-token-auth": "~0.0.21",
+    "angular-translate": "2.4.0",
+    "angular-localization": "~1.0.2"
+  },
+  "devDependencies": {},
+  "resolutions": {
+    "jquery": "2.1.1",
+    "angular": "~1.2.24",
+    "angular-translate": "2.4.0"
+  }
+}
--- src/app/scripts/app.js
+++ src/app/scripts/app.js
@@ -2,131 +2,140 @@
 'use strict';
 
 
-angular.module('app', [
-  'ngRoute',
-  'ngSanitize',
-  'ui.router',
-  'ui.bootstrap',
-  'restangular',
-  'pascalprecht.translate'
+angular.module('mincheeapp', [
+  'ngMaterial',
 ])
-.config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  '$locationProvider',
-  '$translateProvider',
-  function ($stateProvider, $urlRouterProvider, $locationProvider, $translateProvider) {
-    var isDev = (document.URL.search('http://localhost') >= 0) ? true : false;
-//    $locationProvider.html5Mode(true);  
-    $urlRouterProvider.otherwise('/');
-    if (isDev) {
-      $locationProvider.html5Mode(false);  
-      console.log('¡¡¡¡ DEV MODE ON !!!!');
-    }
-    
-    $stateProvider
-    .state('home',{
-      url: '/',
-      templateUrl: 'views/home.html',
-    })
-    .state('about',{
-      url: '/about',
-      templateUrl: 'views/about.html'
-    })
-    .state('workshops', {
-      url: '/workshops',
-      templateUrl: 'views/workshops.html'
-    })
-    .state('workshops.item',{
-      url: '/:wsday',
-      controller: 'WorkshopItemCtrl',
-      templateUrl: 'views/workshop.html'
-    })
-    .state('projects', {
-      url: '/projects',
-      templateUrl: 'views/projects.html'
-    })
-    .state('projects.item',{
-      url: '/:project',
-      controller: 'ProjectCtrl',
-      templateUrl: 'views/project.html'
-    })
-    .state('equipment',{
-      url: '/equipment',
-      templateUrl: 'views/equipment.html'
-    });
-    
-    
-    $translateProvider.translations('en', {
-      welcomeTo: 'Welcome to',
-      mincheeLab: 'Minchee Lab',
-      navHome: 'Home',
-      navAbout: 'About',
-      navWorkshops: 'Workshops',
-      navProjects: 'Projects',
-      navDiscussions: 'Discussions',
-      navEquipment: 'Equipment',
-      joinWorkshop: 'Join a Workshop!',
-      schedule2014: 'the schedule for 2014-2015 is now available, get it while it is hot ;-)',
-      learn: 'Learn',
-      make: 'Make',
-      share: 'Share',
-      learnText: 'Let\'s learn together something new everyday. Life is too short to not discover new things.',
-      makeText: 'Customize stuff, build your own objects and connect them to the Internet of Things.',
-      shareText: 'Because doing alone is no fun and because we are always learning more with others.',
-      quoteGershenfeld: 'Give ordinary people the right tools, and they will design and build the most extraordinary things.',
-      quoteBanzi: '',
-      getInTouch: 'get social with us',
-      workshopsSchedule: 'Workshops Schedule',
-      workshops2014: 'Season 2014/2015',
-      
-    });
-    $translateProvider.translations('zh', {
-      welcomeTo: '歡迎來到',
-      mincheeLab: 'Minchee Lab 勉智實驗室',
-      navHome: '首頁',
-      navWorkshops: '工作坊時間表',
-      navProjects: 'Projects',
-      navDiscussions: 'Discussions',
-      joinWorkshop: '參加我們的工作坊！',
-      schedule2014: '2014 至 2015 的工作坊時間已經推出, 快D黎睇下啦 ;-)',
-      learn: '學習',
-      make: '製作',
-      share: '分享',
-      learnText: '一齊黎參與學習最新既技術！',
-      makeText: '個人化物件，製作屬於自己既用具，仲可以連接物聯網',
-      shareText: '自己一個做唔好玩架，因為我地通過其他人學習知道得更多',
-      quoteGershenfeld: '比一般人士正確的工具，之後他們可以設計，製作非一般既物件',
-      quoteBanzi: '',
-      getInTouch: '聯絡我地啦！',
-      workshopsSchedule: '工作坊時間表',
-      workshops2014: '2014/2015季度',
-    });
-    $translateProvider.preferredLanguage('en');
-//    $translateProvider.determinePreferredLanguage();
-  }
-])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+  .primaryPalette('teal')
+    .accentPalette('pink')
+    .warnPalette('red')
+    .backgroundPalette('grey');
+})
+
+.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.openLeftMenu = function() {
+    $mdSidenav('left').toggle();
+  };
+ 
+}]);
 
 
-.controller('NavBarCtrl', [
-  '$scope',
-  '$rootScope',
-  '$translate',
-  function($scope, $rootScope, $translate) {
-    $rootScope.currentLanguage = $translate.preferredLanguage();
-    $scope.changeLanguage = function (langKey) {
-      $translate.use(langKey);
-      $rootScope.currentLanguage = langKey;
-    };
-  }
-])
+//.config([
+//  '$stateProvider',
+//  '$urlRouterProvider',
+//  '$locationProvider',
+//  '$translateProvider',
+//  function ($stateProvider, $urlRouterProvider, $locationProvider, $translateProvider) {
+//    var isDev = (document.URL.search('http://localhost') >= 0) ? true : false;
+////    $locationProvider.html5Mode(true);  
+//    $urlRouterProvider.otherwise('/');
+//    if (isDev) {
+//      $locationProvider.html5Mode(false);  
+//      console.log('¡¡¡¡ DEV MODE ON !!!!');
+//    }
+//    
+//    $stateProvider
+//    .state('home',{
+//      url: '/',
+//      templateUrl: 'views/home.html',
+//    })
+//    .state('about',{
+//      url: '/about',
+//      templateUrl: 'views/about.html'
+//    })
+//    .state('workshops', {
+//      url: '/workshops',
+//      templateUrl: 'views/workshops.html'
+//    })
+//    .state('workshops.item',{
+//      url: '/:wsday',
+//      controller: 'WorkshopItemCtrl',
+//      templateUrl: 'views/workshop.html'
+//    })
+//    .state('projects', {
+//      url: '/projects',
+//      templateUrl: 'views/projects.html'
+//    })
+//    .state('projects.item',{
+//      url: '/:project',
+//      controller: 'ProjectCtrl',
+//      templateUrl: 'views/project.html'
+//    })
+//    .state('equipment',{
+//      url: '/equipment',
+//      templateUrl: 'views/equipment.html'
+//    });
+//    
+//    
+//    $translateProvider.translations('en', {
+//      welcomeTo: 'Welcome to',
+//      mincheeLab: 'Minchee Lab',
+//      navHome: 'Home',
+//      navAbout: 'About',
+//      navWorkshops: 'Workshops',
+//      navProjects: 'Projects',
+//      navDiscussions: 'Discussions',
+//      navEquipment: 'Equipment',
+//      joinWorkshop: 'Join a Workshop!',
+//      schedule2014: 'the schedule for 2014-2015 is now available, get it while it is hot ;-)',
+//      learn: 'Learn',
+//      make: 'Make',
+//      share: 'Share',
+//      learnText: 'Let\'s learn together something new everyday. Life is too short to not discover new things.',
+//      makeText: 'Customize stuff, build your own objects and connect them to the Internet of Things.',
+//      shareText: 'Because doing alone is no fun and because we are always learning more with others.',
+//      quoteGershenfeld: 'Give ordinary people the right tools, and they will design and build the most extraordinary things.',
+//      quoteBanzi: '',
+//      getInTouch: 'get social with us',
+//      workshopsSchedule: 'Workshops Schedule',
+//      workshops2014: 'Season 2014/2015',
+//      
+//    });
+//    $translateProvider.translations('zh', {
+//      welcomeTo: '歡迎來到',
+//      mincheeLab: 'Minchee Lab 勉智實驗室',
+//      navHome: '首頁',
+//      navWorkshops: '工作坊時間表',
+//      navProjects: 'Projects',
+//      navDiscussions: 'Discussions',
+//      joinWorkshop: '參加我們的工作坊！',
+//      schedule2014: '2014 至 2015 的工作坊時間已經推出, 快D黎睇下啦 ;-)',
+//      learn: '學習',
+//      make: '製作',
+//      share: '分享',
+//      learnText: '一齊黎參與學習最新既技術！',
+//      makeText: '個人化物件，製作屬於自己既用具，仲可以連接物聯網',
+//      shareText: '自己一個做唔好玩架，因為我地通過其他人學習知道得更多',
+//      quoteGershenfeld: '比一般人士正確的工具，之後他們可以設計，製作非一般既物件',
+//      quoteBanzi: '',
+//      getInTouch: '聯絡我地啦！',
+//      workshopsSchedule: '工作坊時間表',
+//      workshops2014: '2014/2015季度',
+//    });
+//    $translateProvider.preferredLanguage('en');
+////    $translateProvider.determinePreferredLanguage();
+//  }
+//])
 
-.controller('WorkshopItemCtrl',[
-  '$scope',
-  '$stateParams',
-  function($scope, $stateParams) {
-    $scope.ws = $stateParams.wsday;
-  }
-])
 
-;
+//.controller('NavBarCtrl', [
+//  '$scope',
+//  '$rootScope',
+//  '$translate',
+//  function($scope, $rootScope, $translate) {
+//    $rootScope.currentLanguage = $translate.preferredLanguage();
+//    $scope.changeLanguage = function (langKey) {
+//      $translate.use(langKey);
+//      $rootScope.currentLanguage = langKey;
+//    };
+//  }
+//])
+
+//.controller('WorkshopItemCtrl',[
+//  '$scope',
+//  '$stateParams',
+//  function($scope, $stateParams) {
+//    $scope.ws = $stateParams.wsday;
+//  }
+//])
